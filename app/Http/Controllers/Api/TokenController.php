2fceed54062a7f5827b5046c71a5ea4e076918a0
Cleiton <cleiton_601@hotmail.com>
2026-01-27 14:26:53 -0300

    feat:(Logout) criando logout e logout-all, revogando tokens

--- app/Http/Controllers/Api/TokenController.php
+++ app/Http/Controllers/Api/TokenController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Auth\LoginRequest;
 
 class TokenController extends Controller
 {
-    public function store(LoginRequest $request)
+    public function store(LoginRequest $request): JsonResponse
     {
         //Valida dados do login
         $credentials = $request->validated();
@@ -30,5 +31,27 @@ class TokenController extends Controller
             "message" => "Login successful",
             "token" => $token
         ], 201);
+    }
+
+    public function destroy(Request $request): JsonResponse
+    {
+        //Remove o token do usuário
+        $request->user()->currentAccessToken()->delete();
+
+        return new JsonResponse([
+            'success' => true,
+            'message' => 'Token revoked successfully'
+        ], 200);
+    }
+
+        public function destroyAll(Request $request): JsonResponse
+    {
+        //Remove o token do usuário
+        $request->user()->tokens()->delete();
+
+        return new JsonResponse([
+            'success' => true,
+            'message' => 'All tokens revoked successfully'
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,22 @@
 use App\Http\Controllers\Api\TokenController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function() {
-    Route::get('/hello', function(){ 
+Route::prefix('v1')->group(function () {
+    Route::get('/hello', function () {
         return ['message' => 'Hello World', 'teste' => 'message teste'];
     });
 
-    Route::post('/login', [TokenController::class, 'store'])->name('login.store');
+    Route::middleware('guest')->group(function () {
+        Route::post('/login', [TokenController::class, 'store'])
+            ->name('login.store');
+    });
 
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [TokenController::class, 'destroy'])
+            ->name('login.destroy');
+
+        Route::post('/logout-all', [TokenController::class, 'destroyAll'])
+            ->name('login.destroyAll');
+    });
 });
-
